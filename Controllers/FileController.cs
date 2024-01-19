10c82c2928acc0c4d1b428a2d6a3d68d2c9c9a31
Darren542 <Darrenluck@icloud.com>
2024-01-19 14:27:26 -0800

    initial commit

---
+++ Controllers/FileController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TextFilesMVC;
+
+public class FileController : Controller
+{
+    private readonly ILogger<FileController> _logger;
+
+    private readonly IWebHostEnvironment _env;
+
+    public FileController(ILogger<FileController> logger, IWebHostEnvironment env)
+    {
+        _logger = logger;
+        _env = env;
+    }
+
+    public IActionResult Index()
+    {
+        // _logger.LogInformation(_env.ContentRootPath);
+
+        var path = Path.Combine(_env.ContentRootPath, "TextFiles");
+        // _logger.LogInformation(path);
+        var files = Directory.GetFiles(path);
+        ViewBag.Files = files;
+        return View();
+    }
+
+}
